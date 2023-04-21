a6e2fe8f492af942f15ed82abd6b7397f0a043f2
Maxik <m.shilko@mail.ru>
2023-04-21 19:36:31 +0500

    Counter added ver1 completed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, flash, redirect, make_response, session
+from flask import Flask, request, render_template, flash, redirect, session
 from data import db_session
 import logging
 from data.submits import Submit
@@ -7,45 +7,35 @@ from datetime import datetime, timedelta
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'M3d1ZWlod0ZFV0pXRkplZnZkbnhzbmN4enVocXd5dWVyMnkzMjh0SUVXSEZFM1VJZWcxMg=='
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=1)
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=60 * 24 * 30)
 logging.basicConfig(level=logging.INFO)
 sessionStorage = {}
-
-
-@app.before_request
-def increment_count():
-    session.permanent = True
-    session.modified = True
-    user_id = request.remote_addr
-    if 'counter' not in session or type(session['counter']) != dict:
-        session['counter'] = {}
-    if user_id not in session['counter']:
-        session['counter'][user_id] = 1
-    else:
-        session['counter'][user_id] += 1
 
 
 @app.route('/')
 @app.route('/main_page')
 def main_page():
-    visits_count = len([k for k in session['counter'].keys() if session['counter'][k] > 0])
-    return render_template('main_page.html', title='Главная страница', count=visits_count)
+    if 'counter' not in session:
+        session['counter'] = 1
+    else:
+        session['counter'] += 1
+    return render_template('main_page.html', title='Главная страница', count=session['counter'])
 
 
 @app.route('/about')
 def about_page():
-    return render_template('about.html', title='О нас')
+    return render_template('about.html', title='О нас', count=session['counter'])
 
 
 @app.route('/services')
 def services_page():
-    return render_template('services.html', title='Услуги')
+    return render_template('services.html', title='Услуги', count=session['counter'])
 
 
 @app.route('/submit', methods=['GET', 'POST'])
 def submit_page():
     if request.method == 'GET':
-        return render_template('submit.html', title='Оставить заявку')
+        return render_template('submit.html', title='Оставить заявку', count=session['counter'])
     elif request.method == 'POST':
         works = ['Внесение изменений', 'Реорганизация ООО', 'Ликвидация',
                  'Регистрация ИП', 'Регистрация юр. лица', 'Регистрация ООО', 'Другое']
@@ -69,7 +59,7 @@ def submit_page():
 
 @app.route('/galery')
 def galery_page():
-    return render_template('galery.html', title='Галерея работ')
+    return render_template('galery.html', title='Галерея работ', count=session['counter'])
 
 
 if __name__ == '__main__':
